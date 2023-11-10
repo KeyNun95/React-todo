f149915f6f2d09327ac2f9f799d6bd7fafab7dba
KeyNun95 <k.nunez2017@gmail.com>
2023-11-10 18:24:59 -0500

    made todoform and added notes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TodoForm from './Components/todoForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>To do app</h1>
+      <TodoForm />
     </div>
   );
 }
--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,16 +1,27 @@
 import React, {useState} from "react";
 
 export default function TodoForm() {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState(''); //set to empty
+
+    const handleChange = e => { //when event occurs
+        setInput(e.target.value); //setInput will be value we type which we are changing from original useState first mentioned above
+    }
+
+    const handleSubmit = e => { //listening for event(event is pushing add todo button)
+        e.preventDefault(); //will stop page from refreshing which is the default action
+
+        setInput(''); //will reset setInput back to empty string after we hit button to trigger event
+    };
 
     return (
-        <form className="todo-form">
+        <form className="todo-form" onSubmit={handleSubmit}>
             <input 
                 type="text" 
                 placeholder="add a todo"
                 value={input}
                 name="text"
                 className="todo-input"
+                onChange={handleChange}
             />
             <button className="todo-button">Add To Do</button>
         </form>
